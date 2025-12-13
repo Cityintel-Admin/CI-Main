5dc3bf88959c53e519963295c9697b4881c458d3
Cityintel-Admin <cityintel2@gmail.com>
2025-12-13 10:37:32 +0000

    Create assets-data.js

---
+++ assets-data.js
@@ -0,0 +1,33 @@
+// Demo company assets for CityIntel.
+// Later you can load this from a backend instead of a static file.
+
+window.CIAssets = [
+  {
+    id: "asset-uk-hq",
+    name: "UK Headquarters",
+    city: "London",
+    country: "United Kingdom",
+    lat: 51.5074,
+    lon: -0.1278,
+    type: "office"
+  },
+  {
+    id: "asset-lon-dc1",
+    name: "London Data Centre 1",
+    city: "London",
+    country: "United Kingdom",
+    lat: 51.515,
+    lon: -0.1,
+    type: "data-centre"
+  },
+  {
+    id: "asset-lagos-plant",
+    name: "Lagos Manufacturing Plant",
+    city: "Lagos",
+    country: "Nigeria",
+    lat: 6.5244,
+    lon: 3.3792,
+    type: "plant"
+  }
+  // Add more assets as needed
+];
